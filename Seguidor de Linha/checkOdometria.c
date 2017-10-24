fd98aa0e42f9df277fa91e4433f8c3edeae32d20
Jhone Darts <jhonedarts@gmail.com>
2017-10-24 16:06:57 -0300

    Supervisao, falta fazer as proporcoes de odometria

---
+++ Seguidor de Linha/checkOdometria.c
@@ -0,0 +1,14 @@
+task main(){
+  wait1Msec(1000);
+  motor[motorB] =0;
+  motor[motorC] =0;
+	nMotorEncoder(motorB) = 0;
+	nMotorEncoder(motorC) = 0;
+	while(nMotorEncoder(motorB) < (160*360)/17.3){
+	 motor[motorB] = 20;                  // Motor B is run at a 60 power level.
+	 motor[motorC] = 20;
+	}
+	motor[motorB] =0;
+  motor[motorC] =0;
+}
+//17.3cm circunferencia
